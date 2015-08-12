29eb0eb3ce9aa9a5de9542f3594a506847bcb610
ramielian <ramielian@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-12 07:30:44 +0000

    minor bug fixes- release 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-missing-functions/trunk@1218874 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- asset/required/rsa.php
+++ asset/required/rsa.php
@@ -3,12 +3,12 @@ echo"
 
 <h3>About the PLugin</h3>
 <div class='inside'>
-<p class='inner'>Configuration:  <a href='http://thainetworks.net/wp-missing-functions/' target='_blank' title='Plugin URL'>Plugin URI</a></p>
+<p class='inner'>Configuration:  <a href='http://thainetworks.net/shop/plugins/wp-missing-functions/' target='_blank' title='Plugin URL'>Plugin URI</a></p>
 <p></p><hr>
 <h4 class='inner'>Do you like this plugin?</h4>
 <p class='inner'>Please, <a href='http://wordpress.org/support/view/plugin-reviews/wp-missing-functions' target='_blank' title='rate it'>rate it</a> on WordPress.org<br>          
 </p><hr>
-<div style='width:auto; margin:auto; text-align:center;'><a href='http://thainetworks.net/wp-missing-functions/' target='_blank'><img width='170' height='70' src='/wp-content/plugins/wp-missing-functions/asset/images/wpmf-pro.png'></a></div>
+<div class='up-pic'><a href='http://thainetworks.net/shop/plugins/wp-missing-functions/' target='_blank'><img width='170' height='70' src='/wp-content/plugins/wp-missing-functions/asset/images/wpmf-pro.png'></a></div>
 </div>
         
         "
